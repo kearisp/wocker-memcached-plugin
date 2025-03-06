aad959c98db5480b6dc94ddd9b4a511d261ebb4b
Kris Papercut <krispcut@gmail.com>
2025-03-06 16:56:06 +0000

    v1.0.7

--- src/makes/Service.ts
+++ src/makes/Service.ts
@@ -1,26 +1,22 @@
-import {
-    Config,
-    ConfigProperties
-} from "@wocker/core";
-
-
-export type ServiceProps = ConfigProperties & {
+export type ServiceProps = {
+    name: string;
     imageName?: string;
     imageVersion?: string;
 };
 
-export class Service extends Config<ServiceProps> {
+export class Service {
+    public name: string;
     public imageName: string;
     public imageVersion: string;
 
     public constructor(props: ServiceProps) {
-        super(props);
-
         const {
+            name,
             imageName = "memcached",
             imageVersion = "latest"
         } = props;
 
+        this.name = name;
         this.imageName = imageName;
         this.imageVersion = imageVersion;
     }
@@ -31,5 +27,13 @@ export class Service extends Config<ServiceProps> {
 
     public get imageTag(): string {
         return `${this.imageName}:${this.imageVersion}`;
+    }
+
+    public toObject(): ServiceProps {
+        return {
+            name: this.name,
+            imageName: this.imageName,
+            imageVersion: this.imageVersion
+        };
     }
 }
